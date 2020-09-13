9f6ed5e04b2feb0e6444a4db8d3590fa498230f6
Richard <Richard@DESKTOP-9LM3OJE>
2020-09-13 15:52:34 -0300

    Adicionar arquivos de projeto.

---
+++ MultiLanguage/Language.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiLanguage
+{
+    public class Language
+    {
+        public string Msg01 { get; set; }
+        public string Msg02 { get; set; }
+        public string Msg03 { get; set; }
+    }
+}
---
+++ MultiLanguage/LanguageConfigurationManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+
+namespace MultiLanguage
+{
+    public class LanguageConfigurationManager
+    {
+        public Language Texts(string lg)
+        {
+            var obj = new Language();
+            if (string.IsNullOrWhiteSpace(lg))
+            {
+                lg = "en-us";
+            }
+
+            obj.Msg01 = ConfigurationManager.AppSettings["01-" + lg];
+            obj.Msg02 = ConfigurationManager.AppSettings["02-" + lg];
+            obj.Msg03 = ConfigurationManager.AppSettings["03-" + lg];
+            return obj;
+        }
+    }
+}
